ea020cb3a089ededb77cf893f91809eb9a3d1a7a
Olle Mattsson <rymdolle@gmail.com>
2023-11-14 11:50:38 +0100

    ovning33.cpp

---
+++ ovning33.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+void initialer(const char* namn)
+{
+  std::cout << *namn;
+  while (*namn++ != ' ');
+  std::cout << *namn;
+}
+
+int main()
+{
+  char* dittNamn = new char[80];
+
+  std::cout << "Ge ditt för- och efternamn: ";
+  std::cin.getline(dittNamn, 80);
+  std::cout << "Hej " << dittNamn << ", dina initialer är: ";
+  initialer(dittNamn);
+  std::cout << '\n';
+
+  char* start = dittNamn;
+  char* end = start;
+  // Hitta nolltecknet
+  for (; *end != '\0'; end++);
+
+  // Skriv ut baklänges
+  std::cout << "Baklänges:\t";
+  for (int i = 1; (end-i) >= start; i++)
+    std::cout << *(end-i);
+  std::cout << '\n';
+
+  // Byt plats på första och sista bokstaven
+  std::cout << "Före byte:\t" << dittNamn << "\n";
+  // char first = *start; // tmp var
+  // char* last = end-1;
+  // *start = *last;
+  // *last = first;
+  std::swap(*start, *(end-1));
+  std::cout << "Efter byte:\t" << dittNamn << "\n";
+
+  delete[] dittNamn;
+}
